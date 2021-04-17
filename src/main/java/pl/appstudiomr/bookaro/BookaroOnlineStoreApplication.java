436848bba8ca9dd9b2a0f189ab605822cf2506a3
Michal Rubajczyk <rubajticos@gmail.com>
2021-04-17 11:05:27 +0200

    Init project

---
+++ src/main/java/pl/appstudiomr/bookaro/BookaroOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package pl.appstudiomr.bookaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookaroOnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookaroOnlineStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/appstudiomr/bookaro/BookaroOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.appstudiomr.bookaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookaroOnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
