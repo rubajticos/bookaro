d02977950c22e983942d82a420972fdba9681954
Michal Rubajczyk <rubajticos@gmail.com>
2021-04-17 11:18:25 +0200

    Create first service and use it - using simple constructor

--- src/main/java/pl/appstudiomr/bookaro/BookaroOnlineStoreApplication.java
+++ src/main/java/pl/appstudiomr/bookaro/BookaroOnlineStoreApplication.java
@@ -1,13 +1,23 @@
 package pl.appstudiomr.bookaro;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class BookaroOnlineStoreApplication {
+public class BookaroOnlineStoreApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookaroOnlineStoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookaroOnlineStoreApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+        // tutaj
+        CatalogService catalogService = new CatalogService();
+        List<Book> books = catalogService.findByTitle("Pan Tadeusz");
+        books.forEach(System.out::println);
+    }
 }
