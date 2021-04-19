dd723785f13254830721fc4c909df0f8a9335713
Michal Rubajczyk <rubajticos@gmail.com>
2021-04-19 20:20:00 +0200

    M2L12 - Injecting primitives.

--- src/main/java/pl/appstudiomr/bookaro/ApplicationStartup.java
+++ src/main/java/pl/appstudiomr/bookaro/ApplicationStartup.java
@@ -1,6 +1,6 @@
 package pl.appstudiomr.bookaro;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import pl.appstudiomr.bookaro.catalog.application.CatalogController;
@@ -9,13 +9,22 @@ import pl.appstudiomr.bookaro.catalog.domain.Book;
 import java.util.List;
 
 @Component
-@RequiredArgsConstructor
 public class ApplicationStartup implements CommandLineRunner {
     private final CatalogController catalogController;
+    private final String title;
+    private final Long limit;
+
+    ApplicationStartup(CatalogController catalogController,
+                       @Value("${bookaro.catalog.query}") String title,
+                       @Value("${bookaro.catalog.limit:3}") Long limit) {
+        this.catalogController = catalogController;
+        this.title = title;
+        this.limit = limit;
+    }
 
     @Override
     public void run(String... args) {
-        List<Book> books = catalogController.findByTitle("Pan");
-        books.forEach(System.out::println);
+        List<Book> books = catalogController.findByTitle(title);
+        books.stream().limit(limit).forEach(System.out::println);
     }
 }
